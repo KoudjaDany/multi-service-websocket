deb8756ff3c5eba5b138b1e7d499176dad1a81bb
Dany Koudja <koudjadany@gmail.com>
2022-09-29 17:59:37 +0200

    Configure and use an  Artemis ActiveMQ as a Stomp Broker Relay for WebSockets.

--- src/main/java/com/training/multiservicewebsocket/config/WebSocketConfiguration.java
+++ src/main/java/com/training/multiservicewebsocket/config/WebSocketConfiguration.java
@@ -2,6 +2,7 @@ package com.training.multiservicewebsocket.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
@@ -18,11 +19,34 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
     @Autowired
     private WebSocketChannelInterceptor channelInterceptor;
 
+    @Value("${broker.host}")
+    private String brokerHost;
+
+    @Value("${broker.port}")
+    private int brokerPort;
+
+    @Value("${broker.username}")
+    private String brokerUser;
+
+    @Value("${broker.password}")
+    private String brokerPassword;
+
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/queue", "/topic");
-       registry.setApplicationDestinationPrefixes("/app");
+        //registry.enableSimpleBroker("/queue", "/topic");
+        registry.enableStompBrokerRelay("/queue", "/topic")
+                .setRelayHost(brokerHost)
+                .setRelayPort(brokerPort)
+                .setClientLogin(brokerUser)
+                .setClientPasscode(brokerPassword)
+                .setSystemLogin(brokerUser)
+                .setSystemPasscode(brokerPassword)
+                .setUserDestinationBroadcast("/topic/unresolved-user")
+                .setUserRegistryBroadcast("/topic/log-user-registry")
+        ;
+
+        registry.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
