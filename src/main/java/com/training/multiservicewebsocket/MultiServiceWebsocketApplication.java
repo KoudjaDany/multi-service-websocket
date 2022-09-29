a12b65f81c8d990c56791630b554ecf3fc01c199
Dany Koudja <koudjadany@gmail.com>
2022-09-29 11:55:33 +0200

    Initial commit

---
+++ src/main/java/com/training/multiservicewebsocket/MultiServiceWebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.training.multiservicewebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiServiceWebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiServiceWebsocketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/training/multiservicewebsocket/MultiServiceWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.multiservicewebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiServiceWebsocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
